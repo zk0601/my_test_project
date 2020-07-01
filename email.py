7adb85e84c6d2df5d94d362aef29941845fe8e8a
zhoukun <zhoukun@ehousechina.com>
2020-07-01 10:02:01 +0800

    fix

--- email.py
+++ email.py
@@ -16,18 +16,18 @@ local_tmp_file = '/var/log/mysql/mysql_slow_query_tmp.log'
 Receivers = ['zhoukun@ehousechina.com']
 
 
-def send_email():
+def send_email(text):
     html = """
     <!DOCTYPE html>
        <html lang="en">
         <head>
             <meta charset="UTF-8">
             <title>yun慢sql日志</title>
-            <h1>复制到编辑器中查看结构数据</h1>
+            %s
         </head>
         <body>
         </body
-    </html>"""
+    </html>""" % text
     mail_host = 'smtp.ehousechina.com'
     mail_user = 'cyjz@ehousechina.com'
     mail_pass = 'jqXxNGyatvDnfE1C'
@@ -43,10 +43,10 @@ def send_email():
     message['Subject'] = Header(subject, 'utf-8')
     message.attach(MIMEText(html, 'html', 'utf-8'))
 
-    att = MIMEText(open(local_tmp_file, 'rb').read(), 'base64', 'utf-8')
-    att["Content-Type"] = 'application/octet-stream'
-    att["Content-Disposition"] = 'attachment; filename="mysql_slow_query.log"'
-    message.attach(att)
+    # att = MIMEText(open(local_tmp_file, 'rb').read(), 'base64', 'utf-8')
+    # att["Content-Type"] = 'application/octet-stream'
+    # att["Content-Disposition"] = 'attachment; filename="mysql_slow_query.log"'
+    # message.attach(att)
 
     smtp = smtplib.SMTP_SSL(mail_host, mail_port)
     smtp.set_debuglevel(True)
@@ -58,23 +58,24 @@ def send_email():
 def main():
     if os.path.exists(local_tmp_file):
         os.remove(local_tmp_file)
-    now = int(time.time()) - 3000
+    now = int(time.time()) - 180
     time_compile = re.compile("SET timestamp=(\d+);")
+    text = ""
     tag = False
-    ff = open(local_tmp_file, 'a')
+    line1, line2 ,line3, line4 = '', '', '', ''
     with open(log_file, 'r') as f:
         for line in f.readlines():
             if not tag:
+                line1, line2, line3, line4 = line, line1, line2, line3
                 if re.search(time_compile, line):
                     line_time = re.search(time_compile, line).group(1)
                     if now < int(line_time):
                         tag = True
             if tag:
-                ff.write(line)
-    ff.close()
-
+                text += "<p>" + line + "</p>"
     if tag:
-        send_email()
+        text = "<p>" + line4 + "</p>" + "<p>" + line3 + "</p>" + "<p>" + line2 + "</p>" + text
+        send_email(text)
 
 
 if __name__ == '__main__':
